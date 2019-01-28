055ca37240145168a6b4417ee62a4d6d51993c1c
Alexander Somov <alexandree.somov@gmail.com>
2019-01-28 11:50:14 +0300

    Add support for precompiled cinterp1d for Windows

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 """Setup.py module."""
 from setuptools import setup, find_packages, Extension
+import platform
+import warnings
+
 try:
     import numpy as np
 except ImportError:
@@ -9,21 +12,30 @@ except ImportError:
     else:
         import pip._internal as internal
         internal.main(['install', 'numpy'])
+        import numpy as np
 
-setup(
-   name='pyrao',
-   version='0.9',
-   description='Toolkit designed to integrate BSA structures \
-                with the most recent world astronomic practices.',
-   license="GNUv3",
-   author='Alexander',
-   # author_email='',
-   # url='',
-   packages=find_packages(),
-   install_requires=['numpy', 'pandas', 'matplotlib', 'angles', 'astropy',
-                     'scipy'],
-   ext_modules=[Extension("pyrao.integration.cinterp1d",
-                          ["pyrao/integration/cinterp1d.c"],
-                          include_dirs=[np.get_include()],
-                          build_dir="pyrao/integration")]
+configuration = dict(
+    name='pyrao',
+    version='0.9',
+    description='Toolkit designed to integrate BSA structures \
+                 with the most recent world astronomic practices.',
+    license="GNUv3",
+    author='Alexander Somov',
+    # author_email='',
+    url='https://github.com/AlexanderBBI144/PyRAO/',
+    packages=find_packages(),
+    install_requires=['numpy', 'pandas', 'matplotlib', 'angles', 'astropy',
+                      'scipy'],
+    # include_package_data=True
 )
+
+
+if platform.system() == "Windows":
+    configuration['include_package_data'] = True
+else:
+    configuration['ext_modules'] = [Extension("pyrao.integration.cinterp1d",
+                                              ["pyrao/integration/cinterp1d.c"],
+                                                include_dirs=[np.get_include()],
+                                                build_dir="pyrao/integration")]
+
+setup(**configuration)
